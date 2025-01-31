618bc327045888f4fe63f37fe399b06d28175b26
coderLady <creativegirl@outlook.fr>
2025-01-31 17:50:48 +0100

    back

--- src/main/java/com/samia/gestion/clients/service/ClientService.java
+++ src/main/java/com/samia/gestion/clients/service/ClientService.java
@@ -99,10 +99,10 @@ public class ClientService {
 //        throw new OtherExceptions( "Les champs sont obligatoires.");
 //    }
     Client client = mapToClient(clientDTO);
-        Optional<Client> existingClient = clientRepository.findByEmail(clientDTO.email());
-        if(existingClient.isPresent()){
-            throw new AlreadyExistsException("l'adresse mail est déja utilisée");
-        }
+//        Optional<Client> existingClient = clientRepository.findByEmail(clientDTO.email());
+//        if(existingClient.isPresent()){
+//            throw new AlreadyExistsException("Utilisateur existe déja");
+//        }
         if (clientDTO.email() != null && clientDTO.email().trim().isEmpty()) {
             client.setEmail(null);
         }
