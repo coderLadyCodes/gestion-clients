670a67920282deb88e5ffb87f3f63339be7a298c
coderLady <creativegirl@outlook.fr>
2025-02-18 11:19:24 +0100

    back

--- src/main/java/com/samia/gestion/clients/exception/GlobalExceptionHandler.java
+++ src/main/java/com/samia/gestion/clients/exception/GlobalExceptionHandler.java
@@ -16,6 +16,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public @ResponseBody Map<String, String> getExceptionHandler(MethodArgumentNotValidException exception){
+        exception.printStackTrace();
         Map<String, String> errors = new HashMap<>();
         exception.getBindingResult()
                 .getFieldErrors()
@@ -27,36 +28,42 @@ public class GlobalExceptionHandler {
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler({NotFoundException.class})
     public  @ResponseBody ErrorDetails notHoundException(NotFoundException exception) {
+        exception.printStackTrace();
         return new ErrorDetails(exception.getMessage(), "NOT_FOUND");
     }
 
     @ResponseStatus(CONFLICT)
     @ExceptionHandler({AlreadyExistsException.class})
     public  @ResponseBody ErrorDetails alreadyExistsException(AlreadyExistsException exception) {
+        exception.printStackTrace();
         return new ErrorDetails(exception.getMessage(), "ALREADY_EXISTS");
     }
 
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(OtherExceptions.class)
     public @ResponseBody ErrorDetails handleOtherException(OtherExceptions exception) {
+        exception.printStackTrace();
         return new ErrorDetails(exception.getMessage(), "BAD_REQUEST");
     }
 
     @ResponseStatus(UNAUTHORIZED)
     @ExceptionHandler(UnauthorizedException.class)
     public @ResponseBody ErrorDetails handleUnauthorizedException(UnauthorizedException exception) {
+        exception.printStackTrace();
         return new ErrorDetails( exception.getMessage(), "UNAUTHORIZED");
     }
 
     @ResponseStatus(INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public @ResponseBody ErrorDetails handleGenericException(Exception exception) {
+        exception.printStackTrace();
         return new ErrorDetails("An unexpected error occurred.", "INTERNAL_SERVER_ERROR");
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public @ResponseBody ErrorDetails handleAccessDeniedException(AccessDeniedException exception) {
+        exception.printStackTrace();
         return new ErrorDetails("Access denied: " + exception.getMessage(), "FORBIDDEN");
     }
 
